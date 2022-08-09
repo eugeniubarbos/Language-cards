1c7d5affe187d54642348293021c4cc6ed85d6b0
peewwie <93018127+peewwie@users.noreply.github.com>
2022-08-09 18:58:51 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+import CardList from './components/CardList/CardList';
+
+function App() {
+  return (
+    <div className="App">
+  
+      <Header />
+      <CardList/>
+    </div>
+  );
+}
+
+export default App;
